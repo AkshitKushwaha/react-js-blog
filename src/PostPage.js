569a18decfb0cf1242be3561a97a07f38752d3d8
Akshit Kushwaha <id.akshitkushwaha@gmail.com>
2024-03-05 20:05:04 +0530

    Created a simple blog project with react js

---
+++ src/PostPage.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {useParams, Link} from 'react-router-dom';
+
+export default function PostPage({posts, handleDelete}) {
+
+  const {id} = useParams();
+  const post = posts.find(post => (post.id).toString() === id);
+  return (
+    <main className="PostPage">
+        <article className="post">
+            {post &&
+             <>
+              <h2>{post.title}</h2>
+              <p className='postDate'>{post.datetime}</p>
+              <p className='postBody'>{post.body}</p>
+              <button onClick={() => handleDelete(post.id)}>
+                  Delete Post
+              </button>
+             </>
+             }
+             {
+                  !post && (
+                      <h1>
+                          Post not found. Return to <Link to="/">Home</Link>
+                      </h1>
+                  )
+             }
+        </article>
+
+    </main>
+  )
+}
